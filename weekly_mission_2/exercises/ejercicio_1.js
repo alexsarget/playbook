28a0e86469f1c7f28cb320c5067779326861a88e
Alejandro Tonatiuh <alexsarget@gmail.com>
2022-04-17 23:33:06 -0500

    Aprendiendo a modificar un archivo desde la terminal

--- weekly_mission_2/exercises/ejercicio_1.js
+++ weekly_mission_2/exercises/ejercicio_1.js
@@ -17,6 +17,25 @@ const whatsapp={
     
     getGeneralinf: function(){return `This User ${this.user} have ${this.contactos} contacts`},
 }
+    console.log("INFORMACIÓN DEL WHATSAPP")
     console.log("Nombre del usuario " + whatsapp.name)
     console.log(whatsapp.getGeneralinf())
     console.log(whatsapp.getTotalnotf())
+
+    const estados={
+        user:"UserName",
+        mi_hist:2,
+        contacto:"Nombre_Cont",
+        cant:4,
+
+
+    
+        getnotf: function() { return `The user ${this.user}  have ${this.mi_hist} storys `},
+        
+        getContinf: function(){return `Your friend ${this.contacto} have ${this.cant} storys`},
+    }
+        console.log("INFORMACIÓN DE LOS ESTADOS")
+        console.log("Nombre del usuario " + estados.user)
+        console.log(whatsapp.getGeneralinf())
+        console.log(whatsapp.getTotalnotf())
+
