31cbf95c049fb68ed2acbe88b1817e0273a6187e
Alejandro Tonatiuh <53270727+alexsarget@users.noreply.github.com>
2022-04-19 23:22:46 -0500

    Actualizar carpeta de la segunda semana

---
+++ weekly_mission_2/prueba.js
@@ -0,0 +1,10 @@
+const productos=[
+    {id: 'kljnkjbkjkjbkjb', nombre:'Camiseta', precio:500},
+    {id: 'iuguvuivoiugyihg', nombre:'Pantalon', precio:2000},
+    {id: 'nbvmnbnbvmnbv', nombre:'Calcetin', precio:1500},
+]
+
+const idProdcutos=productos.map(({id})=>id); 
+
+console.log(productos)
+console.log(idProdcutos)
