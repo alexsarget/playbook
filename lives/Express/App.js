bc5be304fce678ec2020c9ff66ae879bab3eb37f
Alejandro Tonatiuh <53270727+alexsarget@users.noreply.github.com>
2022-04-22 14:15:45 -0500

    Agregando ejercicio

--- lives/Express/App.js
+++ lives/Express/App.js
@@ -13,6 +13,14 @@ app.get('/launchx',(req,res)=>{
     res.send("Binevenidos a LaunchX")
 })
 
+app.get('/node',(req,res)=>{
+    const explorer1={id:1, name:"Explorer"}
+    const explorer2={id:2, name:"Explorer"}
+    const explorer3={id:3, name:"Explorer"}
+    const explorer4={id:4, name:"Explorer"}
+    res.send([explorer1, explorer2, explorer3,explorer4])
+})
+
 app.listen(port,()=>{
     console.log("Server Listo")
 })
