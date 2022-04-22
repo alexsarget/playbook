7dc6ff619a97fdeeb17be14ea2d74234df621a8b
Alejandro Tonatiuh <53270727+alexsarget@users.noreply.github.com>
2022-04-22 15:25:24 -0500

    Agregando carpta de trabajos Express

--- lives/Express/App.js
+++ lives/Express/App.js
@@ -1,6 +1,9 @@
-const express= require("express")
-const app=express()
-const port =3000 //localhost:3000
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
 
 // Esto va a responder a localhost 3000
 app.get('/',(req,res)=>{
